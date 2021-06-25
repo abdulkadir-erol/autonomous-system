73702131903c795a9bbf87f95cea7e6e3481e3cf
Abdulkadir Erol <abdulkadirerol66@gmail.com>
2021-06-25 14:59:25 +0300

    addition and deletion of the data table is done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 var clicks = 0;
 var progressInterval;
-var timeTotal;
-
+var timeTotal = 0;
+var profileNum = 1;
+var dataTable = document.createElement('table');
 
 var profiles = new Array(8);
 /* var profile1;
@@ -21,8 +22,8 @@ let setRowNo = '0';
 let setBeginTime = '00.00';
 let setEndTime = '00.00'
 let setRepeatCount = '1';
-let setMethod = '0';
-let setNextProfile = '1';
+let setMethod = '1';
+let setNextProfile = '0';
 let setTimeTypeValue = 'SEC';
 
 function setup() {
@@ -32,6 +33,22 @@ function setup() {
     for (profile in profiles) {
         profile = true;
     }
+
+    /* Setup the table headers */
+    const r0 = document.createElement('tr');
+    const h1 = document.createElement('th');
+    h1.textContent = 'Index';
+    r0.appendChild(h1);
+    const h2 = document.createElement('th');
+    h2.textContent = "Time";
+    r0.appendChild(h2);
+    const h3 = document.createElement('th');
+    h3.textContent = "Power";
+    r0.appendChild(h3);
+
+    dataTable.appendChild(r0);
+    setRowNo++;
+    console.log("row: ", setRowNo);
 }
 
 $('#auto_switch').on('change', function() {
@@ -58,6 +75,7 @@ $('#time_switch').on('change', function() {
 });
 
 function openProfile(evt, profileName, num) {
+    profileNum = num;
     console.log("Entered Profile", num);
     setProfileNum = num;
     var i, tabcontent, tablinks;
@@ -81,147 +99,125 @@ function delProfile() {
     alert("Deletion will be done");
 }
 
-function gProfile(profileNum) {
+/* function gProfile(profileNum) {
     $('.right').show();
     switch (profileNum) {
         case 1:
-            getProfileTable(1);
+            addToProfileTable(1);
             break;
         case 2:
-            getProfileTable(2);
+            addToProfileTable(2);
             break;
         case 3:
-            getProfileTable(3);
+            addToProfileTable(3);
             break;
         case 4:
-            getProfileTable(4);
+            addToProfileTable(4);
             break;
         case 5:
-            getProfileTable(5);
+            addToProfileTable(5);
             break;
         case 6:
-            getProfileTable(6);
+            addToProfileTable(6);
             break;
         case 7:
-            getProfileTable(7);
+            addToProfileTable(7);
             break;
         case 8:
-            getProfileTable(8);
+            addToProfileTable(8);
             break;
         default:
             document.getElementById('errorMsg').innerHTML = "Invalid Profile Entry";
             $('.error').show();
     }
-}
+} */
 
-async function getProfileTable(num) {
-    let str = document.getElementById("rowNumber" + num).value;
-    setRowNo = str;
-    if (str <= 0 || str > 12) {
-        document.getElementById('errorMsg').innerHTML = "Invalid Value";
-        $('.error').show();
-    } else {
-        $('.error').hide();
-        let totalTime = 0;
-        const dataTable = document.createElement('table');
-        const r0 = document.createElement('tr');
-        const h1 = document.createElement('th');
-        h1.textContent = 'Index';
-        r0.appendChild(h1);
-        const h2 = document.createElement('th');
-        h2.textContent = "Time";
-        r0.appendChild(h2);
-        const h3 = document.createElement('th');
-        h3.textContent = "Power";
-        r0.appendChild(h3);
+async function addToProfileTable(num) {
+    let timeAdded = document.getElementById("addTime" + num).value;
+    let powerAdded = document.getElementById("addPower" + num).value;
 
-        if (clicks == 0) {
-            totalTime = 0;
-            dataTable.appendChild(r0);
+    $('.error').hide();
+    setTimeAdded = timeAdded;
+    setPowerAdded = powerAdded;
+    if (setRowNo <= 12) {
+        if (timeAdded <= 0 || timeAdded > 60) {
+            document.getElementById('errorMsg').innerHTML = "Time Value must be between 0-60";
+            $('.error').show();
+        } else if (powerAdded <= 0 || powerAdded > 5) {
+            document.getElementById('errorMsg').innerHTML = "Power Value must be between 0-5";
+            $('.error').show();
+        } else {
+            $('.right').show();
+            $('.error').hide();
 
-            for (let i = 0; i < str; i++) {
+            let r1 = document.createElement('tr');
+            let td1 = document.createElement('td');
+            //td1.textContent = data[i].todo;
+            //td1.textContent = timePowerData[i].time;
+            td1.textContent = setRowNo;
+            r1.appendChild(td1);
 
-                let r1 = document.createElement('tr');
-                let td1 = document.createElement('td');
-                //td1.textContent = data[i].todo;
-                //td1.textContent = timePowerData[i].time;
-                td1.textContent = i + 1;
-                r1.appendChild(td1);
+            let td2 = document.createElement('td');
+            //td2.textContent = data[i].deadline;
+            //td2.textContent = timePowerData[i].power;
+            td2.textContent = timeAdded;
+            timeTotal += parseInt(td2.textContent);
 
-                let td2 = document.createElement('td');
-                //td2.textContent = data[i].deadline;
-                //td2.textContent = timePowerData[i].power;
-                td2.textContent = 10 * i + 20;
-                totalTime += parseInt(td2.textContent);
-                r1.appendChild(td2);
+            console.log(timeTotal);
+            r1.appendChild(td2);
 
-                let td3 = document.createElement('td');
-                //td2.textContent = data[i].deadline;
-                //td2.textContent = timePowerData[i].power;
-                td3.textContent = 25 * i;
-                r1.appendChild(td3);
+            let td3 = document.createElement('td');
+            //td2.textContent = data[i].deadline;
+            //td2.textContent = timePowerData[i].power;
+            td3.textContent = powerAdded;
+            r1.appendChild(td3);
 
-                dataTable.appendChild(r1);
-            }
-            clicks += 1;
-        } else if (clicks > 0) {
-            totalTime = 0;
-            let temp = table.rows.length;
-            for (let i = 0; i < temp; i++) {
-                $('tr:last-child').remove();
-            }
-
-            dataTable.appendChild(r0);
-
-            for (let i = 0; i < str; i++) {
-
-                let r1 = document.createElement('tr');
-                let td1 = document.createElement('td');
-                //td1.textContent = data[i].todo;
-                //td1.textContent = timePowerData[i].time;
-                td1.textContent = i + 1;
-                r1.appendChild(td1);
-
-                let td2 = document.createElement('td');
-                //td2.textContent = data[i].deadline;
-                //td2.textContent = timePowerData[i].power;
-                td2.textContent = 10 * i + 20;
-                totalTime += parseInt(td2.textContent);
-                r1.appendChild(td2);
-
-                let td3 = document.createElement('td');
-                //td2.textContent = data[i].deadline;
-                //td2.textContent = timePowerData[i].power;
-                td3.textContent = 25 * i;
-                r1.appendChild(td3);
-
-                dataTable.appendChild(r1);
-            }
-        }
-        table = document.getElementById('jsonTable' + num).appendChild(dataTable);
-        document.getElementById('jsonTable' + num).appendChild(dataTable);
-        progress(totalTime, $('#progressBar' + num));
-    }
-
-    $(function() {
-        $("#jsonTable" + num).sortable({
-            items: 'tr:not(tr:first-child)',
-            cursor: 'pointer',
-            axis: 'y',
-            dropOnEmpty: false,
-            start: function(e, ui) {
-                ui.item.addClass("selected");
-            },
-            stop: function(e, ui) {
-                ui.item.removeClass("selected");
-                $(this).find("tr").each(function(index) {
-                    if (index > 0) {
-                        $(this).find("td").eq(0).html(index);
+            dataTable.appendChild(r1);
+            setRowNo++;
+            $(function() {
+                $("#jsonTable" + num).sortable({
+                    items: 'tr:not(tr:first-child)',
+                    cursor: 'pointer',
+                    axis: 'y',
+                    dropOnEmpty: false,
+                    start: function(e, ui) {
+                        ui.item.addClass("selected");
+                    },
+                    stop: function(e, ui) {
+                        ui.item.removeClass("selected");
+                        $(this).find("tr").each(function(index) {
+                            if (index > 0) {
+                                $(this).find("td").eq(0).html(index);
+                            }
+                        });
                     }
                 });
-            }
-        });
-    });
+            });
+        }
+    } else {
+        document.getElementById('errorMsg').innerHTML = "Row Number must be between 0-12";
+        $('.error').show();
+    }
+    table = document.getElementById('jsonTable' + num).appendChild(dataTable);
+    document.getElementById('jsonTable' + num).appendChild(dataTable);
+}
+
+async function deleteFromProfileTable(num) {
+    $('.right').show();
+    if (setRowNo >= 1) {
+        $('.error').hide();
+        let value = (setRowNo) * 3 - 1;
+        timeTotal -= $('tr:last-child').find('td:nth-child(2)').html();
+        $('tr:last-child').remove();
+        setRowNo--;
+        console.log(timeTotal);
+        console.log("row: ", setRowNo);
+    } else {
+        document.getElementById('errorMsg').innerHTML = "There should be data to be deleted";
+        $('.error').show();
+    }
+    table = document.getElementById('jsonTable' + num).appendChild(dataTable);
+    document.getElementById('jsonTable' + num).appendChild(dataTable);
 
 }
 
@@ -238,19 +234,24 @@ function progress(timetotal, $element) {
         timeleft = timeleft - 1;
         if (timeleft == 0) {
             clearInterval(progressInterval);
-            return gProfile(2);
         }
     }, 1000);
 };
 
-var beginResult, endResult;
+var beginResult = 0,
+    endResult = 0;
 
 async function setForBegin() {
-
     let begin = document.getElementById("begin").value;
     let beginH = parseInt(begin[0] + begin[1]);
     let beginM = parseInt(begin[3] + begin[4]);
-    beginResult = beginH * 60 + beginM;
+    if ((Number.isInteger(beginH))) {
+        beginResult = beginH * 60 + beginM;
+        $('.error').hide();
+    } else {
+        document.getElementById('errorMsg').innerHTML = "The beginning time is not valid";
+        $('.error').show();
+    }
 }
 
 async function setForEnd() {
@@ -258,13 +259,19 @@ async function setForEnd() {
     let end = document.getElementById("end").value;
     let endH = parseInt(end[0] + end[1]);
     let endM = parseInt(end[3] + end[4]);
-    endResult = endH * 60 + endM;
+    if ((Number.isInteger(endH))) {
+        endResult = endH * 60 + endM;
+        $('.error').hide();
+    } else {
+        document.getElementById('errorMsg').innerHTML = "The beginning time is not valid";
+        $('.error').show();
+    }
 }
 
 function controlForTime() {
     let totalMin = endResult - beginResult;
-    if (endResult < beginResult) {
-        document.getElementById('errorMsg').innerHTML = "The ending time cannot be earlier than the beginning time";
+    if (endResult <= beginResult) {
+        document.getElementById('errorMsg').innerHTML = "The ending time cannot be earlier than or equal to the beginning time";
         $('.error').show();
     } else if (totalMin < timeTotal) {
         document.getElementById('errorMsg').innerHTML = "The total time cannot be longer than the profile period ";
@@ -276,17 +283,9 @@ function controlForTime() {
     }
 }
 
-
-
 async function getRepeatCount() {
     let repeat = document.getElementById("repeatCount").value;
     setRepeatCount = repeat;
-
-}
-
-function getNextProfile() {
-    let str = document.getElementById("nextProfile").value;
-    return gProfile(str);
 }
 
 function getMethod() {
@@ -301,12 +300,13 @@ function getNextProfile() {
 
 function submitResult() {
     controlForTime();
-    result = 'AS';
+    progress(timeTotal, $('#progressBar' + profileNum));
+    result = '#AS';
     result = result.concat(setSwitchValue);
     result = result.concat('#P');
     result = result.concat(setProfileNum);
     result = result.concat('#ROW');
-    result = result.concat(setRowNo);
+    result = result.concat(setRowNo - 1);
     result = result.concat('#B');
     result = result.concat(setBeginTime);
     result = result.concat('#E');
