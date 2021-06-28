1bd0c91b9796cd3e5165f66cf8ca1532f113091d
Abdulkadir Erol <abdulkadirerol66@gmail.com>
2021-06-28 18:18:43 +0300

    database is created

--- public/main.js
+++ public/main.js
@@ -1,13 +1,25 @@
+var profiles = [];
+var profile = {
+    id: 1,
+    table: [{
+        index: "Index",
+        time: "Time",
+        power: "Power"
+    }],
+    beginTime: "",
+    endTime: "",
+    timeType: "",
+    methodType: 0,
+    nextProfile: 0,
+    repeatCount: 1
+};
+
 var clicks = 0;
 var progressInterval;
 var timeTotal = 0;
 var profileNum = 1;
 var dataTable = document.createElement('table');
 var setTable = '';
-
-var profiles = new Array(8);
-
-profiles = "";
 
 var result = "";
 let setSwitchValue = '0';
@@ -21,13 +33,22 @@ let setNextProfile = '0';
 let setTimeTypeValue = 'SEC';
 
 function setup() {
+    //showProfile();
     $('.right').hide();
     $('.middle').hide();
     $('.error').hide();
-    for (profile in profiles) {
-        profile = true;
-    }
+}
 
+/* function getProfileObject(id) {
+    for (let i = 0; i < profiles.length; i++) {
+        if (id == profiles[i].id) {
+            return profiles[i]
+        }
+    }
+    return false;
+} */
+
+function defaultTable() {
     /* Setup the table headers */
     const r0 = document.createElement('tr');
     const h1 = document.createElement('th');
@@ -42,8 +63,9 @@ function setup() {
 
     dataTable.appendChild(r0);
     setRowNo++;
-    console.log("row: ", setRowNo);
 }
+
+
 
 $('#auto_switch').on('change', function() {
     if ($(this).is(':checked')) {
@@ -69,7 +91,10 @@ $('#time_switch').on('change', function() {
 });
 
 function openProfile(evt, profileName, num) {
+    $('.right').show();
+    defaultTable();
     profileNum = num;
+    //showProfile(profileNum);
     console.log("Entered Profile", num);
     setProfileNum = num;
     var i, tabcontent, tablinks;
@@ -84,14 +109,6 @@ function openProfile(evt, profileName, num) {
     document.getElementById(profileName).style.display = "block";
     evt.currentTarget.className += " active";
 };
-
-function addProfile(profileNum) {
-    alert("Addition will be done");
-}
-
-function delProfile() {
-    alert("Deletion will be done");
-}
 
 async function addToProfileTable(num) {
     let timeAdded = document.getElementById("addTime" + num).value;
@@ -108,7 +125,6 @@ async function addToProfileTable(num) {
             document.getElementById('errorMsg').innerHTML = "Power Value must be between 0-5";
             $('.error').show();
         } else {
-            $('.right').show();
             $('.error').hide();
 
             let r1 = document.createElement('tr');
@@ -166,7 +182,6 @@ async function deleteFromProfileTable(num) {
     $('.right').show();
     if (setRowNo >= 1) {
         $('.error').hide();
-        let value = (setRowNo) * 3 - 1;
         timeTotal -= $('tr:last-child').find('td:nth-child(2)').html();
         $('tr:last-child').remove();
         setRowNo--;
@@ -215,7 +230,6 @@ async function setForBegin() {
 }
 
 async function setForEnd() {
-
     let end = document.getElementById("end").value;
     let endH = parseInt(end[0] + end[1]);
     let endM = parseInt(end[3] + end[4]);
@@ -228,25 +242,56 @@ async function setForEnd() {
     }
 }
 
+var timeAdded = 0;
+async function checkTime(num) {
+    timeAdded = document.getElementById("addTime" + num).value;
+    if (timeAdded <= 0 || timeAdded > 60) {
+        document.getElementById('errorMsg').innerHTML = "Time Value must be between 0-60";
+        $('.error').show();
+    } else {
+        $('.error').hide();
+    }
+}
+
+var powerAdded = 0;
+async function checkPower(num) {
+    powerAdded = document.getElementById("addPower" + num).value;
+    if (powerAdded <= 0 || powerAdded > 5) {
+        document.getElementById('errorMsg').innerHTML = "Power Value must be between 0-5";
+        $('.error').show();
+    } else {
+        $('.error').hide();
+    }
+}
+
+
 function controlForTime() {
     let totalMin = endResult - beginResult;
     if (endResult <= beginResult) {
         document.getElementById('errorMsg').innerHTML = "The ending time cannot be earlier than or equal to the beginning time";
         $('.error').show();
+        return false;
     } else if (totalMin < timeTotal) {
         document.getElementById('errorMsg').innerHTML = "The total time cannot be longer than the profile period ";
         $('.error').show();
+        return false;
     } else {
         $('.error').hide();
         setBeginTime = document.getElementById("begin").value;
         setEndTime = document.getElementById("end").value;
+        return true;
     }
 }
 
 function controlForTable() {
+    setTable = '';
     let tableRowLength = dataTable.rows.length;
-    console.log("table row length", tableRowLength);
     for (let i = 1; i < tableRowLength; i++) {
+        profile.table.push({
+            index: i,
+            time: tableValue = dataTable.rows[i].cells[1].innerHTML,
+            power: tableValue = dataTable.rows[i].cells[2].innerHTML
+        });
         for (let j = 0; j < 3; j++) {
             var tableValue = dataTable.rows[i].cells[j].innerHTML;
             setTable = setTable + tableValue + ":";
@@ -256,7 +301,7 @@ function controlForTable() {
 }
 
 async function setRepeatCount() {
-    let repeat = document.getElementById("repeatCount").value;
+    repeat = document.getElementById("repeatCount").value;
     if (repeat < 0 || repeat > 12) {
         document.getElementById('errorMsg').innerHTML = "Repeat must be between 0-12";
         $('.error').show();
@@ -276,31 +321,167 @@ function getNextProfile() {
     setNextProfile = nextProfileNo;
 }
 
-function submitResult() {
+async function submitResult() {
     controlForTime();
-    controlForTable();
-    progress(timeTotal, $('#progressBar' + profileNum));
-    result = '#AS';
-    result = result.concat(setSwitchValue);
-    result = result.concat('#P');
-    result = result.concat(setProfileNum);
-    result = result.concat('#ROW');
-    result = result.concat(setRowNo - 1);
-    result = result.concat('#DATA');
-    result = result.concat(setTable);
-    result = result.concat('B');
-    result = result.concat(setBeginTime);
-    result = result.concat('#E');
-    result = result.concat(setEndTime);
-    result = result.concat('#M');
-    result = result.concat(setMethod);
-    result = result.concat('#NP');
-    result = result.concat(setNextProfile);
-    result = result.concat('#R');
-    result = result.concat(setRepeat);
-    result = result.concat('#');
-    result = result.concat(setTimeTypeValue);
-    console.log(result);
+    if (controlForTime()) {
+        controlForTable();
+        progress(timeTotal, $('#progressBar' + profileNum));
+        result = '#AS';
+        result = result.concat(setSwitchValue);
+        result = result.concat('#P');
+        result = result.concat(setProfileNum);
+        profile.id = setProfileNum;
+        result = result.concat('#ROW');
+        result = result.concat(setRowNo - 1);
+        result = result.concat('#DATA');
+        result = result.concat(setTable);
+        result = result.concat('B');
+        result = result.concat(setBeginTime);
+        profile.beginTime = setBeginTime;
+        result = result.concat('#E');
+        result = result.concat(setEndTime);
+        profile.endTime = setEndTime;
+        result = result.concat('#M');
+        result = result.concat(setMethod);
+        profile.methodType = setMethod;
+        result = result.concat('#NP');
+        result = result.concat(setNextProfile);
+        profile.nextProfile = setNextProfile;
+        result = result.concat('#R');
+        result = result.concat(setRepeat);
+        profile.repeatCount = setRepeat;
+        result = result.concat('#');
+        result = result.concat(setTimeTypeValue);
+        profile.timeType = setTimeTypeValue;
+        profiles.push(profile);
+        console.log("ps", profiles);
+        /* localStorage.setItem('profiles', JSON.stringify(profiles)); */
+        const data = { profile };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+
+        const response = await fetch('/autonomous', options);
+        const json = await response.json();
+        console.log(json);
+
+    } else {
+        console.log("There are some problems with the values entered");
+    }
 }
+
+async function getProfile() {
+    const response = await fetch('/autonomous');
+    const data = await response.json();
+    console.log(data);
+
+    document.getElementById("begin").value = data[0].profile.beginTime;
+    document.getElementById("end").value = data[0].profile.endTime;
+    document.getElementById("select").value = data[0].profile.methodType;
+    document.getElementById("nextProfile").value = data[0].profile.nextProfile;
+    document.getElementById("repeatCount").value = data[0].profile.repeatCount;
+    document.getElementById('jsonTable1').value = data[0].profile.table;
+    document.getElementById("time_switch").value = data[0].profile.timeType;
+}
+
+//getProfile();
+
+/* async function getData() {
+    const response = await fetch('/autonomous');
+    const data = await response.json();
+
+    const todoTable = document.createElement('table');
+    const r0 = document.createElement('tr');
+    const h1 = document.createElement('th');
+    h1.textContent = 'AutoSwitch';
+    r0.appendChild(h1);
+    const h2 = document.createElement('th');
+    h2.textContent = "ProfileNo";
+    r0.appendChild(h2);
+    const h25 = document.createElement('th');
+    h25.textContent = "RowNo";
+    r0.appendChild(h25);
+    const h3 = document.createElement('th');
+    h3.textContent = "TableValues";
+    r0.appendChild(h3);
+    const h4 = document.createElement('th');
+    h4.textContent = "Begin";
+    r0.appendChild(h4);
+    const h5 = document.createElement('th');
+    h5.textContent = "End";
+    r0.appendChild(h5);
+    const h6 = document.createElement('th');
+    h6.textContent = "Method";
+    r0.appendChild(h6);
+    const h7 = document.createElement('th');
+    h7.textContent = "NextProfile";
+    r0.appendChild(h7);
+    const h8 = document.createElement('th');
+    h8.textContent = "RepeatCount";
+    r0.appendChild(h8);
+    const h9 = document.createElement('th');
+    h9.textContent = "TimeType";
+    r0.appendChild(h9);
+
+    todoTable.appendChild(r0);
+
+    console.log(data.length);
+
+    for (let i = 0; i < data.length; i++) {
+        let r1 = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.textContent = data[i].setSwitchValue;
+        r1.appendChild(td1);
+
+        let td2 = document.createElement('td');
+        td2.textContent = data[i].setProfileNum;
+        r1.appendChild(td2);
+
+        let td25 = document.createElement('td');
+        td25.textContent = data[i].setRowNo;
+        r1.appendChild(td25);
+
+        let td3 = document.createElement('td');
+        td3.textContent = data[i].setTable;
+        r1.appendChild(td3);
+
+        let td4 = document.createElement('td');
+        td4.textContent = data[i].setBeginTime;
+        r1.appendChild(td4);
+
+        let td5 = document.createElement('td');
+        td5.textContent = data[i].setEndTime;
+        r1.appendChild(td5);
+
+        let td6 = document.createElement('td');
+        td6.textContent = data[i].setMethod;
+        r1.appendChild(td6);
+
+        let td7 = document.createElement('td');
+        td7.textContent = data[i].setNextProfile;
+        r1.appendChild(td7);
+
+        let td8 = document.createElement('td');
+        td8.textContent = data[i].setRepeat;
+        r1.appendChild(td8);
+
+        let td9 = document.createElement('td');
+        td9.textContent = data[i].setTimeTypeValue;
+        r1.appendChild(td9);
+
+        todoTable.appendChild(r1);
+    }
+
+    document.getElementById('table2').appendChild(todoTable);
+    //document.body.append(todoTable);
+} */
+
+getProfile();
 
 setup();
