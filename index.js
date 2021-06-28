d869c7b0b6254f7115d95d94e3dd60c14a8b2597
Abdulkadir Erol <abdulkadirerol66@gmail.com>
2021-06-28 18:16:47 +0300

    database added

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require('express');
+const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening in 3000'));
+app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.get('/autonomous', (request, response) => {
-    console.log('I got to get something !');
     database.find({}, (err, data) => {
         if (err) {
             response.end();
             return;
         }
         response.json(data);
-    })
+    });
 });
 
 app.post('/autonomous', (request, response) => {
-    console.log('I got a request ');
     const data = request.body;
-
-    response.json({
-        status: 'success',
-        time: data.time,
-        power: data.power
-    });
+    database.insert(data);
+    response.json(data);
 });
